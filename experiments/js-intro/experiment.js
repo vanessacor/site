41e1ac2186985c3ce3d6461d4ba718d832861fd1
Vanessacor <vanessapcbarros@gmail.com>
2019-02-27 17:47:30 +0100

    add odin test

--- experiments/js-intro/experiment.js
+++ experiments/js-intro/experiment.js
@@ -32,3 +32,27 @@ buttonB.addEventListener ('click', () => {
     imageA.hidden = false;
     imageB.hidden = true;
 })
+
+//odin project - fundamentals 3
+
+//add 
+function add7(num1) {
+    let result = num1 + 7;
+    return result;
+}   
+
+//multiply
+function multiply (num1, num2) {
+    let result = num1 * num2;
+    return result
+}
+
+//capitalize first letter
+function capitalize (string) {
+    return string.charAt (0).toUpperCase() + string.slice(1);
+}
+
+//return last letter
+function lastLetter (string) {
+    return string.substr(string.length -1)
+}
