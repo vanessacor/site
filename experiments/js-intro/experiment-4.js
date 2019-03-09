59c48498116017eb13b65821087054233bdde56a
Vanessacor <vanessapcbarros@gmail.com>
2019-03-09 11:53:25 +0100

    change experiment-4

--- experiments/js-intro/experiment-4.js
+++ experiments/js-intro/experiment-4.js
@@ -2,25 +2,22 @@
 //when login button is clicked:
 //validate name input
 //validate pass input
-//ok = no msg invalid = required
-//
+//ok = no msg 
+// invalid = required msg
+
 
 function login() {
     let userName = document.getElementById('name').value;
     let userPass = document.getElementById('pass').value;
-    if(!userName =='' && !userPass ==''){
-        document.getElementsByClassName('feedback').innerHTML = 'Required';
-        return false;
-      } else if(userName == ''){
-            document.getElementById('namefeedback').innerHTML = 'Required';
-            return false;
-      } else if(userPass == ''){
-            document.getElementById('passfeedback').innerHTML = 'Required';
-            return false;
-      } else if(userPass.length < 6){
-            document.getElementById('passfeedback').innerHTML = 'minimum 6 character';
-            return false;
-        }
+    if(userName ==''){
+          document.getElementById('namefeedback').innerHTML = 'Required';
+      }     
+    if(userPass == ''){
+          document.getElementById('passfeedback').innerHTML = 'Required';
+      } 
+    if(userPass.length < 6){
+          document.getElementById('passfeedback').innerHTML = 'minimum 6 characters';
+      } 
 }
 
 document.getElementById('loginbtn').addEventListener('click', login);
