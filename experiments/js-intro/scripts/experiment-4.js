3bfd36d7e928287d26c083973d150fcedad002fa
Vanessacor <vanessapcbarros@gmail.com>
2019-03-15 23:03:34 +0100

    fix indentation

--- experiments/js-intro/scripts/experiment-4.js
+++ experiments/js-intro/scripts/experiment-4.js
@@ -9,15 +9,15 @@ const userLogin= document.getElementById('loginbtn');
 userLogin.addEventListener('click', login);
 
 function login() {
-    let userName = document.getElementById('name').value;
-    let userPass = document.getElementById('pass').value;
-    if(userName ==''){
-          document.getElementById('namefeedback').style.display = 'block';
-      }     
-    if(userPass == ''){
-          document.getElementById('passfeedback').style.display = 'block';
-      } 
-    if(userPass.length < 6){
-          document.getElementById('lengthfeedback').style.display = 'block';
-      } 
+  let userName = document.getElementById('name').value;
+  let userPass = document.getElementById('pass').value;
+  if (userName == '') {
+    document.getElementById('namefeedback').style.display = 'block';
+  }     
+  if (userPass == '') {
+    document.getElementById('passfeedback').style.display = 'block';
+  } 
+  if (userPass.length < 6) {
+    document.getElementById('lengthfeedback').style.display = 'block';
+  } 
 }
