2f81a9136008df50abbe78ebc0993aeb6fa16fca
Vanessacor <vanessapcbarros@gmail.com>
2019-05-24 18:14:01 +0200

    add colour function

--- experiments/etch-a-sketch/scripts/script.js
+++ experiments/etch-a-sketch/scripts/script.js
@@ -1,16 +1,13 @@
 'use strict'
 
-// create a function that will create the number of divs (sq) requested;
-// create a function that will colour black the squares or random colour !== black
-  //coding train
-  //el hipnogato curioso & jack sparrot
-
-
 const input = document.getElementById('grid-size');
-input.addEventListener('input', creatGrid);
 const container = document.getElementById('grid-container');
 const resetBtn = document.getElementById('reset');
+const colourBtn = document.getElementById('colour');
+
 resetBtn.addEventListener('click', reset);
+input.addEventListener('input', creatGrid);
+colourBtn.addEventListener('click', changeColor);
 
 function creatGrid(){
   const gridSize = document.getElementById('grid-size').value;
@@ -22,24 +19,49 @@ function creatGrid(){
   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 }  
 
-function createSquare () {
+function createSquare() {
   const square = document.createElement('div');
   square.setAttribute('class', 'square');
-  square.onmouseover = function () {
-  square.style.backgroundColor = 'black';
-  };
-  return square;
+  if (colourBtn.value == 'colour off') {
+    square.addEventListener ('mouseover', () => { 
+      square.style.backgroundColor = 'black'
+    });
+    return square;
+  }
+  else {
+    square.addEventListener ('mouseover', () => { 
+      square.style.backgroundColor = generateColor();
+    });
+    return square;
+  }
+} 
+
+function changeColor() {
+  reset();
+  if (colourBtn.value == 'colour off') {
+    colourBtn.value = 'colour on';
+  }
+  else {
+    colourBtn.value = 'colour off';
+  }
 }
 
-function cleanGrid () {
+function generateColor() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  return bgColor;
+  }
+
+function cleanGrid() {
   while(container.firstChild) {
     container.removeChild(container.firstChild);
   }
 }
 
-function reset () {
+function reset() {
   input.value = '';
   cleanGrid();
 }
-
 
