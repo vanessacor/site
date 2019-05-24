bb85b05e248aa02f5b807552f894e29e45de1013
Vanessacor <vanessapcbarros@gmail.com>
2019-05-24 09:39:14 +0200

    add function etch-a-sketch

--- experiments/etch-a-sketch/scripts/script.js
+++ experiments/etch-a-sketch/scripts/script.js
@@ -2,29 +2,44 @@
 
 // create a function that will create the number of divs (sq) requested;
 // create a function that will colour black the squares or random colour !== black
-  //
+  //coding train
+  //el hipnogato curioso & jack sparrot
 
 
-const input = document.getElementById('grid-size')
+const input = document.getElementById('grid-size');
 input.addEventListener('input', creatGrid);
+const container = document.getElementById('grid-container');
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', reset);
 
 function creatGrid(){
-  const container = document.getElementById('grid-container');
   const gridSize = document.getElementById('grid-size').value;
-  for (let i = 0; i < gridSize; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'squares');
-    container.appendChild(div);
+  cleanGrid();
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    container.appendChild(createSquare());
   }
   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 }  
 
-function sketch() {
-  const squares = document.querySelectorAll('squares');
-  squares.forEach ((e) => {
-    e.addEventListener('mouseover', ()=>{
-      squares.style.background = 'black';
-    })
-  })
+function createSquare () {
+  const square = document.createElement('div');
+  square.setAttribute('class', 'square');
+  square.onmouseover = function () {
+  square.style.backgroundColor = 'black';
+  };
+  return square;
 }
+
+function cleanGrid () {
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+function reset () {
+  input.value = '';
+  cleanGrid();
+}
+
+
