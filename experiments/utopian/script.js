468f70d6d7f37c4fcbe06de378393e31d23c3c5b
Vanessacor <vanessapcbarros@gmail.com>
2019-06-22 19:23:08 +0200

    refactor JS function to single responsibility functions

--- experiments/utopian/script.js
+++ experiments/utopian/script.js
@@ -1,18 +1,41 @@
 'use strict'
 
+const dayNames = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
-window.addEventListener('load', newDate);
-
-function newDate () {
-const dayNames = new Array("Sunday","Monday","Tuesday","Wednesday",
-"Thursday","Friday","Saturday");
-const monthNames = new Array(
-"January","February","March","April","May","June","July",
-"August","September","October","November","December");
-let now = new Date();
-let today = (dayNames[now.getDay()] + ", " + 
-now.getDate() + " " + monthNames[now.getMonth()]
-+ ", " + now.getFullYear());
-document.getElementById('time').innerHTML=today;
+function formatDate (date) {
+  const dayOfTheWeek =  dayNames[date.getDay()];
+  const day = date.getDate();
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+  return `${dayOfTheWeek}, ${day} ${month}, ${year}`;
 }
 
+function main () {
+  const now = new Date();
+  const today = formatDate(now); 
+  document.getElementById('time').innerHTML = today;
+}
+
+window.addEventListener('load', main);
