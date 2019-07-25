a804418086a40ee361341e2ca69b0c169af467d6
Vanessacor <vanessapcbarros@gmail.com>
2019-07-25 21:59:55 +0200

    simplify: calculate only expressions of 2; refactor: extract conditions to functions

--- experiments/calculator/calculator-ui.js
+++ experiments/calculator/calculator-ui.js
@@ -6,12 +6,19 @@ const operatorButtons = document.querySelectorAll('.operator');
 const expressionScreen = document.getElementById('operations');
 const resultScreen = document.getElementById('result');
 
+function updateOperations () {
+  expressionScreen.textContent = expressionParts.join('');
+}
 
+function updateResult (result) {
+  resultScreen.textContent = result;
+}
 
-  
 key.forEach((button)=> {
   button.addEventListener ('click', () => {
-    processKey(button.value);
+    const result = processKey(button.value);
+    updateOperations();
+    updateResult(result);
   })
 });
 
--- experiments/calculator/calculator.js
+++ experiments/calculator/calculator.js
@@ -2,42 +2,12 @@
 
 let expressionParts = [];
 
-
- function createEquation () {
-   if (expressionParts.length <= 3) {
-     let a = expressionParts[0];
-     let op = expressionParts[1];
-     let b = expressionParts[2]
-     calculate(a, op, b)
-   }
-
-   else {
-    let calculateEquation = expressionParts.slice(-2);
-      let a = result;
-      let op = calculateEquation[0];
-      let b = calculateEquation[1]
-      calculate (a, op, b)
-   }
- }
-
-// function createEquation () {
-//   let calculateEquation = expressionParts.slice(-2);
-//   let a = result;
-//   let op = calculateEquation[0];
-//   let b = calculateEquation[1]
-//   calculate (a, op, b)
-// }
-
-
-let result = 0;
-
-
 const operators = [ '+', '-', '*', '/']
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
 
 //Operations
 function add (a, b) {
-  return +a + +b; // can I used this? 
+  return a + b;
 }
 
 function subtract (a, b) {
@@ -60,72 +30,117 @@ function divide (a, b) {
 function calculate (a, op, b) {
   switch (op) {
     case '+':
-      result = add(a, b);
-      break;
-    
+      return add(a, b);
     case '-':
-      result = subtract(a, b);
-      break;
-    
+      return subtract(a, b);
     case '*':
-      result = multiply(a, b);
-      break;
-    
+      return multiply(a, b);
     case '/':
-      result =  divide(a, b);    
+      return  divide(a, b);    
   }
+}
 
-  return result
+function computeResult (expression) {
 
+  let a = Number(expression[0]);
+  let op = expression[1];
+  let b = Number(expression[2]);
+  let result = calculate(a, op, b);
+
+  return result;
+}
+
+
+function isSymbolOperatorAndLastElementOperator(symbol, lastElement) {
+  return operators.includes(symbol) && operators.includes(lastElement);
+}
+
+function isSymbolOperatorAndLastElementNumber(symbol, lastElement) {
+  return operators.includes(symbol) && !operators.includes(lastElement);
+}
+
+function isSymbolNumberAndLastElementOperatorOrNothing(symbol, lastElement) {
+  return !operators.includes(symbol) && operators.includes(lastElement) || lastElement === undefined;
+}
+
+function isSymbolNumberAndLastElementNumber(symbol, lastElement) {
+  return numbers.includes(symbol) && lastElement && !operators.includes(lastElement);
 }
 
 function changeOperators (symbol){
   expressionParts.pop();
   expressionParts.push(symbol);
+}
 
+function addExpressionPart(symbol) {
+  expressionParts.push(symbol);
+}
+
+function modifyLastExpressionPart(lastElement, symbol) {
+  const newNumb = lastElement.concat(symbol);
+  expressionParts[expressionParts.length - 1] = newNumb;
 }
 
 
 function processKey (symbol) {  
+  let result;
+
   const lastElement = expressionParts[expressionParts.length - 1];
+
+  // TODO if symbol is . and last element CONTAINS a dot
   if (symbol === '.' && lastElement === '.') {
     return;
   }
   
-  if (numbers.includes(symbol) && !operators.includes(lastElement) && lastElement) {
+  if (isSymbolNumberAndLastElementNumber(symbol, lastElement)) {
     console.log('2nd if');
-    const newNumb = lastElement.concat(symbol);
-    expressionParts[expressionParts.length - 1] = newNumb;
+    modifyLastExpressionPart(lastElement, symbol);
+    if (expressionParts.length === 3) {
+      result = computeResult(expressionParts);
+    } 
   }
 
-  if (operators.includes(lastElement) && !operators.includes(symbol) || lastElement === undefined) {
+  if (isSymbolNumberAndLastElementOperatorOrNothing(symbol, lastElement)) {
     console.log('3rd if');
-    expressionParts.push(symbol);
+    addExpressionPart(symbol);
+    if (expressionParts.length === 3) {
+      result = computeResult(expressionParts);
+    } 
   }
 
-  if (operators.includes(symbol) && !operators.includes(lastElement)) {
+  if (isSymbolOperatorAndLastElementNumber(symbol, lastElement)) {
     console.log('4th if');
-    createEquation();
-    //does the last operation. Now to figure it out how to replace or where to put it
-    expressionParts.push(symbol);
+    if (expressionParts.length < 3) {
+      addExpressionPart(symbol);
+    } else {
+      result = computeResult(expressionParts);
+    }
   };
 
-  if (operators.includes(symbol) && operators.includes(lastElement)) {
+  if (isSymbolOperatorAndLastElementOperator(symbol, lastElement)) {
     console.log('5th');
     changeOperators(symbol);
-    
   }
+
+  // if symbol is BS and last element number
+  //   modify last element, if last element is now empty pop it from expression
+  //   if expresson still has length 3, compute
+
+  // if symbol is BS and last element operator
+  // pop it
 
   if (symbol === '=' && operators.includes(lastElement)) {
     console.log('error');  
   }
 
   if (symbol === '=') {
-    createEquation()
+    result = computeResult(expressionParts);
+    expressionParts = [String(result)];
+    result = undefined;
   }
+
+  return result;
 }
-
-
 
 
 
