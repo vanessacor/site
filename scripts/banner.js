09b8c8e24c5df8b13de2987affbb45c3be15f373
Vanessacor <vanessapcbarros@gmail.com>
2020-08-19 15:52:45 +0200

    change the href on banner to new website and add input numbers on the js-intro

--- scripts/banner.js
+++ scripts/banner.js
@@ -1,23 +1,22 @@
-'use strict';
+"use strict";
 
-function addStyle (link) {
-  const stylesLink = document.createElement('link');
+function addStyle(link) {
+  const stylesLink = document.createElement("link");
   stylesLink.href = link;
-  stylesLink.setAttribute('rel', 'stylesheet');
-  stylesLink.setAttribute('type', 'text/css');
+  stylesLink.setAttribute("rel", "stylesheet");
+  stylesLink.setAttribute("type", "text/css");
   document.head.appendChild(stylesLink);
 }
 
-function createBanner () {
-  const banner = document.createElement('div');
-  banner.setAttribute('class', 'banner-wrapper');
+function createBanner() {
+  const banner = document.createElement("div");
+  banner.setAttribute("class", "banner-wrapper");
   document.body.appendChild(banner);
-  banner.innerHTML =
-  `<div id="banner">
-    <a href="https://vanessacor.github.io/site/">Vanessa<br>Poppe</a>
+  banner.innerHTML = `<div id="banner">
+    <a href="http://vanessapoppe.me">Vanessa<br>Poppe</a>
   </div>`;
-  addStyle('https://vanessacor.github.io/site/styles/banner.css');
-  addStyle('https://fonts.googleapis.com/css?family=Raleway&display=swap');
+  addStyle("https://vanessacor.github.io/site/styles/banner.css");
+  addStyle("https://fonts.googleapis.com/css?family=Raleway&display=swap");
 }
 
-window.addEventListener('load', createBanner);
+window.addEventListener("load", createBanner);
