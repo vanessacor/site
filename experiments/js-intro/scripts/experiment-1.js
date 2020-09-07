58564ec42eaafb73a096513445267d93afc7c684
Vanessacor <vanessapcbarros@gmail.com>
2020-09-07 21:38:28 +0200

    upgrade eslint and fix linting

--- experiments/js-intro/scripts/experiment-1.js
+++ experiments/js-intro/scripts/experiment-1.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
 // when the user clicks on a button
 // invoke the function runExperiment1
-function runExperiment1() {
-  const age1 = document.getElementById("number1");
-  const age2 = document.getElementById("number2");
-  const age3 = document.getElementById("number2");
+function runExperiment1 () {
+  const age1 = document.getElementById('number1');
+  const age2 = document.getElementById('number2');
+  const age3 = document.getElementById('number2');
   const average =
     (Number(age1.value) + Number(age2.value) + Number(age3.value)) / 3;
 
-  const resultElement = document.getElementById("result-1");
-  resultElement.innerHTML = "The Average is " + average;
+  const resultElement = document.getElementById('result-1');
+  resultElement.innerHTML = 'The Average is ' + average;
 }
